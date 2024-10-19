fcb9ce22a3bf6d909e08e57c0bb7c648058a2860
京颖 杜 <1095168498@qq.com>
2024-10-19 16:49:20 +0800

    完成了整个单体项目的服务拆分工作，初步实现了微服务架构——20241019

--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -1,11 +1,11 @@
 package com.hmall.cart.service.impl;
 
-import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.api.client.ItemClient;
+import com.hmall.api.dto.ItemDTO;
 import com.hmall.cart.domain.dto.CartFormDTO;
-import com.hmall.cart.domain.dto.ItemDTO;
 import com.hmall.cart.domain.po.Cart;
 import com.hmall.cart.domain.vo.CartVO;
 import com.hmall.cart.mapper.CartMapper;
@@ -16,13 +16,7 @@ import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Collection;
 import java.util.List;
@@ -45,9 +39,11 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
 
     // private final IItemService itemService;
 
-    private final RestTemplate restTemplate;
+//    private final RestTemplate restTemplate;
+//
+//    private final DiscoveryClient discoveryClient;
 
-    private final DiscoveryClient discoveryClient;
+    private final ItemClient itemClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -90,33 +86,38 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
     private void handleCartItems(List<CartVO> vos) {
         // 1.获取商品id TODO 处理商品信息
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
-        // 2.查询商品
-        // 第一步首先要根据服务的名称去获取到服务的实例
-        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
-        if(CollUtils.isEmpty(instances)) {
-            return;
-        }
-        // 第二步，手写负载均衡（无敌了
-        ServiceInstance serviceInstance = instances.get(RandomUtil.randomInt(instances.size()));
-        // 用基于 restTemplate 来实现，而不是基于 itemService 。
-        // 利用 RestTemplate 发起http请求，并得到http响应
-        // 字节码里泛型会被擦除，但如果new一个对象那么这个对象的返回值里的泛型还在
-        // CollUtils.join 作用是把itemIds集合里的数据，以逗号","作为分隔符，拼接成一个字符串
-        ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
-                serviceInstance.getUri() + "/items?ids={ids}",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<ItemDTO>>() {
-                },
-                Map.of("ids", CollUtils.join(itemIds, ","))
-        );
-        // 解析响应
-        // 通过响应状态码来判断发送和接收http请求是否成功。响应码200代表成功，所以是is2xxSuccessful()
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            // 查询失败，直接结束
-            return;
-        }
-        List<ItemDTO> items = response.getBody();
+//        // 2.查询商品
+//        // 第一步首先要根据服务的名称去获取到服务的实例
+//        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+//        if(CollUtils.isEmpty(instances)) {
+//            return;
+//        }
+//        // 第二步，手写负载均衡（无敌了
+//        ServiceInstance serviceInstance = instances.get(RandomUtil.randomInt(instances.size()));
+//        // 用基于 restTemplate 来实现，而不是基于 itemService 。
+//        // 利用 RestTemplate 发起http请求，并得到http响应
+//        // 字节码里泛型会被擦除，但如果new一个对象那么这个对象的返回值里的泛型还在
+//        // CollUtils.join 作用是把itemIds集合里的数据，以逗号","作为分隔符，拼接成一个字符串
+//        ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
+//                serviceInstance.getUri() + "/items?ids={ids}",
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<List<ItemDTO>>() {
+//                },
+//                Map.of("ids", CollUtils.join(itemIds, ","))
+//        );
+//        // 解析响应
+//        // 通过响应状态码来判断发送和接收http请求是否成功。响应码200代表成功，所以是is2xxSuccessful()
+//        if (!response.getStatusCode().is2xxSuccessful()) {
+//            // 查询失败，直接结束
+//            return;
+//        }
+//        List<ItemDTO> items = response.getBody();
+
+        // 一行代码搞定查询操作
+        List<ItemDTO> items = itemClient.queryItemByIds(itemIds);
+
+
 
         if (CollUtils.isEmpty(items)) {
             throw new BadRequestException("购物车中商品不存在！");
